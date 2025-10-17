4b29794d4ea952573a09591b7dbb393288231448
jay-kim <electruc@naver.com>
2025-10-17 18:02:46 +0900

    feat. app splash screen customized

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
 import { Stack } from "expo-router";
 import * as SecureStore from "expo-secure-store";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { Animated, StyleSheet, useColorScheme, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export interface User {
   id: string;
@@ -21,8 +27,23 @@ export const AuthContext = createContext<{
   updateUser?: (user: User) => void;
 }>({});
 
-export default function RootLayout() {
+function AnimatedAppLoader({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
   const [user, setUser] = useState<User | null>(null);
+  const [isSplashReady, setIsSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.loadAsync(image);
+      setIsSplashReady(true);
+    }
+    prepare();
+  }, [image]);
 
   const login = () => {
     fetch("/login", {
@@ -57,9 +78,13 @@ export default function RootLayout() {
     ]);
   };
 
-  const updateUser = (user: User) => {
+  const updateUser = (user: User | null) => {
     setUser(user);
-    AsyncStorage.setItem("user", JSON.stringify(user));
+    if (user) {
+      AsyncStorage.setItem("user", JSON.stringify(user));
+    } else {
+      AsyncStorage.removeItem("user");
+    }
   };
 
   useEffect(() => {
@@ -71,8 +96,98 @@ export default function RootLayout() {
     // todo: verify access token
   }, []);
 
+  if (!isSplashReady) {
+    return null;
+  }
+
   return (
     <AuthContext value={{ user, login, logout, updateUser }}>
+      <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>
+    </AuthContext>
+  );
+}
+
+function AnimatedSplashScreen({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
+  const [isAppReady, setIsAppReady] = useState(false);
+  const [isSplashAnimationComplete, setIsSplashAnimationComplete] =
+    useState(false);
+  const animation = useRef(new Animated.Value(1)).current;
+  const { updateUser } = useContext(AuthContext);
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1500,
+        useNativeDriver: true,
+      }).start(() => setIsSplashAnimationComplete(true));
+    }
+  }, [animation, isAppReady]);
+
+  const onImageLoaded = async () => {
+    try {
+      await Promise.all([
+        AsyncStorage.getItem("user").then((user) => {
+          if (user) {
+            updateUser?.(user ? JSON.parse(user) : null);
+          }
+          // todo: verify access token
+        }),
+      ]);
+      await SplashScreen.hideAsync();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsAppReady(true);
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colorScheme === "dark" ? "#101010" : "#ffffff",
+      }}
+    >
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: colorScheme === "dark" ? "#101010" : "#ffffff",
+            opacity: animation,
+          }}
+        >
+          <Animated.Image
+            source={image}
+            style={{
+              resizeMode: "contain",
+              width: Constants.expoConfig?.splash?.imageWidth,
+              height: Constants.expoConfig?.splash?.imageWidth,
+              tintColor: colorScheme === "dark" ? "#ffffff" : undefined,
+            }}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <AnimatedAppLoader image={require("../assets/images/threads-logo.png")}>
       <StatusBar style="auto" animated={true} translucent={false} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
@@ -84,6 +199,6 @@ export default function RootLayout() {
           }}
         />
       </Stack>
-    </AuthContext>
+    </AnimatedAppLoader>
   );
 }
